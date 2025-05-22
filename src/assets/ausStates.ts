c1ef2f4f10708c72561fe4d1d32620bd3990d18e
Nicolas Pardo Machett's <94014352+npardom@users.noreply.github.com>
2025-05-22 14:53:52 -0500

    Create ausStates.ts

---
+++ src/assets/ausStates.ts
@@ -0,0 +1,14 @@
+export interface States {
+  [key: string]: string;
+}
+
+export const states: States = {
+  "nsw": "New South Wales",
+    "nt": "Northern Territory",
+    "qld": "Queensland",
+    "sa": "South Australia",
+    "tas": "Tasmania",
+    "vic": "Victoria",
+    "wa": "Western Australia",
+    "act": "Australian Capital Territory",
+};
